2e0012a931db69e54d91a0b584addb9add2606b0
Akhilesh Kontham <akontham@unomaha.edu>
2017-09-16 14:24:07 -0500

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-test
+                    Laravel-test-test
                 </div>
 
                 <div class="links">
