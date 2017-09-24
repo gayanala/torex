77b56b41798567f011d855077d5fd21ba26a2ce5
fsogoyou <31752406+fsogoyou@users.noreply.github.com>
2017-09-24 13:12:26 -0500

    required fields added

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,71 +1,72 @@
+
 @extends('app')
 @section('content')
-    <h1>Update Profile</h1>
-
-    @if(count($errors) > 0)
-        @foreach($errors->all() as $error)
-            <div class="alert alert-danger">{{$error}}</div>
-        @endforeach
-    @endif
-
-    @if(session('response'))
-        <div class="col-md-8 alert alert-success">
-            {{@session('response')}}
-        </div>
-    @endif
+    <h3>Update Profile</h3>
 
     {!! Form::model($user,['method' => 'PATCH','route'=>['users.update',$user->id]]) !!}
 
-    <div class="form-group">
-        {!! Form::label('First Name', 'First Name:') !!}
-        {!! Form::text('first_name',null,['class'=>'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('Last Name', 'Last Name:') !!}
-        {!! Form::text('last_name',null,['class'=>'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('Email', 'Email:') !!}
-        {!! Form::text('email',null,['class'=>'form-control']) !!}
-    </div>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
 
     <div class="form-group">
-        {!! Form::label('Street Address', 'Street Address 1:') !!}
-        {!! Form::text('street_address1',null,['class'=>'form-control']) !!}
+        {!! Form::label('First Name', '* First Name:') !!}
+        {!! Form::text('first_name',null,['required'],['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('Last Name', '* Last Name:') !!}
+        {!! Form::text('last_name',null,['required'],['class'=>'form-control'] ) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('Email', '* Email:') !!}
+        {!! Form::text('email',null,['required'],['class'=>'form-control']) !!}
+    </div>
+
+
+    <div class="form-group">
+        {!! Form::label('Street Address1', '* Street Address 1:') !!}
+        {!! Form::text('street_address1',null,['required'],['class'=>'form-control']) !!}
     </div>
     <div class="form-group">
-        {!! Form::label('Street Address', 'Street Address 2:') !!}
+        {!! Form::label('Street Address2', 'Street Address 2:') !!}
         {!! Form::text('street_address2',null,['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('City', 'City:') !!}
-        {!! Form::text('city',null,['class'=>'form-control']) !!}
+        {!! Form::label('City', '* City:') !!}
+        {!! Form::text('city',null,['required'],['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('State', 'State:') !!}
-        {!! Form::text('state',null,['class'=>'form-control' ]) !!}
+        {!! Form::label('State', '* State:') !!}
+        {!! Form::text('state',null,['required'],['class'=>'form-control' ]) !!}
 
 
     </div>
     <div class="form-group">
-        {!! Form::label('Zip Code', 'Zip Code:') !!}
-        {!! Form::text('zipcode',null,['class'=>'form-control']) !!}
+        {!! Form::label('Zip Code', '* Zip Code:') !!}
+        {!! Form::text('zipcode',null,['required'],['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('Phone Number', 'Phone Number:') !!}
-        {!! Form::text('phone_number',null,['class'=>'form-control']) !!}
+        {!! Form::label('Phone Number', '* Phone Number:') !!}
+        {!! Form::text('phone_number',null,['required'],['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
         {!! Form::submit('Cancel', ['class' => 'btn btn-primary']) !!}
 
-        <h2>Thank You</h2>
+        <h3>Thank You</h3>
     </div>
     {!! Form::close() !!}
 @stop
