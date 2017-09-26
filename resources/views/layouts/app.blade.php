13255e934e92b911b80c3f8e6f64e2b5987c961a
gayanala <gayanala@unomaha.edu>
2017-09-26 08:32:43 -0500

    added logic to display menus on landing page and on login, signup pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,13 +46,13 @@
                     @guest
                         <li><a href="{{ route('login') }}">Login</a></li>
                         <li><a href="{{ route('register') }}">Register</a></li>
-                        <li><a href="users">Update Profile</a></li>
                         <li><a href="donationrequest">Request Donation</a></li>
                         @else
+                            <li><a href="users">Update Profile</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
                                    aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->first_name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
