b7ee1a7a3a78b8871530e92dd6de0758ddf580dc
gayanala <gayanala@unomaha.edu>
2017-09-26 12:53:50 -0500

    made the edit profile page consistent with other pages

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,74 +1,85 @@
-@extends('app')
+@extends('layouts.app')
 @section('content')
-    <h2>Update Profile</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Update Profile</div>
 
-    <h5>All fields marked with an asterisk (*) are required</h5>
+                    <div class="panel-body">
 
-    {!! Form::model($user,['method' => 'PATCH','route'=>['users.update',$user->id]]) !!}
+                        {!! Form::model($user,['method' => 'PATCH','route'=>['users.update',$user->id], 'class' => 'form-horizontal']) !!}
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <div class="form-group">
+                            {!! Form::label('First Name', '* First Name:', ['class'=>'col-md-4 control-label', ]) !!}
+                            <div class="col-lg-6">
+                                {!! Form::text('first_name',null,['required'], ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('Last Name', '* Last Name:', ['class'=>'col-md-4 control-label']) !!}
+                            <div class="col-lg-6">
+                                {!! Form::text('last_name',null,['required'], ['class' => 'form-control'] ) !!}</div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('Email', '* Email:', ['class'=>'col-md-4 control-label']) !!}
+                            <div class="col-lg-6">{!! Form::text('email',null,['required'],['class'=>'form-control']) !!}</div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('Street Address1', '* Street Address 1:', ['class'=>'col-md-4 control-label']) !!}
+                            <div class="col-lg-6">{!! Form::text('street_address1',null,['required'],['class'=>'form-control']) !!}</div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('Street Address2', 'Street Address 2:', ['class'=>'col-md-4 control-label']) !!}
+                            <div class="col-lg-6">   {!! Form::text('street_address2', null,[''],['class'=>'form-control']) !!}</div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('City', '* City:', ['class'=>'col-md-4 control-label']) !!}
+                            <div class="col-lg-6">{!! Form::text('city',null,['required'],['class'=>'form-control']) !!}</div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('State', '* State:', ['class'=>'col-md-4 control-label']) !!}
+                            <div class="col-lg-6">{!! Form::text('state',null,['required'],['class'=>'form-control' ]) !!}</div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('Zip Code', '* Zip Code:', ['class'=>'col-md-4 control-label']) !!}
+                            <div class="col-lg-6"> {!! Form::text('zipcode',null,['required'],['class'=>'form-control']) !!}</div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('Phone Number', '* Phone Number:', ['class'=>'col-md-4 control-label']) !!}
+                            <div class="col-lg-6">{!! Form::text('phone_number',null,['required'],['class'=>'form-control']) !!}</div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                {!! Form::submit('Update', ['class' => 'btn btn-primary']) !!}
+                                {!! Form::submit('Cancel', ['class' => 'btn btn-primary']) !!}
+                                <span style="color: red"> <h5>Fields Marked With (<span
+                                                style="color: red; font-size: 20px; vertical-align:middle;">*</span>) Are Mandatory</h5></span>
+                            </div>
+                        </div>
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
         </div>
-    @endif
-
-    <div id="wrapper_1"; style="text-align: center"; aria-colcount="2">
-
-        <div class="form-group"; >
-            <div style="width:50%" align="left">  {!! Form::label('First Name', '* First Name:') !!} </div>
-            <div style="width:50%" align="left">       {!! Form::text('first_name',null,['required'],['class'=>'form-control']) !!}</div>
     </div>
-
-    <div class="form-group">
-        <div style="width:50%" align="left">  {!! Form::label('Last Name', '* Last Name:') !!}</div>
-        <div style="width:50%" align="left">  {!! Form::text('last_name',null,['required'],['class'=>'form-control'] ) !!}</div>
-    </div>
-
-    <div class="form-group">
-        <div style="width:50%" align="left">  {!! Form::label('Email', '* Email:') !!}</div>
-        <div style="width:50%" align="left">   {!! Form::text('email',null,['required'],['class'=>'form-control']) !!}</div>
-    </div>
-
-
-    <div class="form-group">
-        <div style="width:50%" align="left">    {!! Form::label('Street Address1', '* Street Address 1:') !!}</div>
-        <div style="width:50%" align="left">    {!! Form::text('street_address1',null,['required'],['class'=>'form-control']) !!}</div>
-    </div>
-    <div class="form-group">
-        <div style="width:50%" align="left">  {!! Form::label('Street Address2', 'Street Address 2:') !!}</div>
-        <div style="width:50%" align="left">   {!! Form::text('street_address2', null,[''],['class'=>'form-control']) !!}</div>
-    </div>
-
-    <div class="form-group">
-        <div style="width:50%" align="left"> {!! Form::label('City', '* City:') !!}</div>
-        <div style="width:50%" align="left"> {!! Form::text('city',null,['required'],['class'=>'form-control']) !!}</div>
-    </div>
-
-    <div class="form-group">
-        <div style="width:50%" align="left"> {!! Form::label('State', '* State:') !!}</div>
-        <div style="width:50%" align="left"> {!! Form::text('state',null,['required'],['class'=>'form-control' ]) !!}</div>
-
-    </div>
-    <div class="form-group">
-        <div style="width:50%" align="left"> {!! Form::label('Zip Code', '* Zip Code:') !!}</div>
-        <div style="width:50%" align="left"> {!! Form::text('zipcode',null,['required'],['class'=>'form-control']) !!}</div>
-    </div>
-
-    <div class="form-group">
-        <div style="width:50%" align="left"> {!! Form::label('Phone Number', '* Phone Number:') !!}</div>
-        <div style="width:50%" align="left"> {!! Form::text('phone_number',null,['required'],['class'=>'form-control']) !!}</div>
-    </div>
-
-    <div class="form-group">
-        {!! Form::submit('Update', ['class' => 'btn btn-primary']) !!}
-        {!! Form::submit('Cancel', ['class' => 'btn btn-primary']) !!}
-
-        <h3>Thank You</h3>
-    </div>
-    </div>
-    {!! Form::close() !!}
-@stop
+@endsection
