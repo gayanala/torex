7893156680005ae65b3cbbbbf9203903dd59e2f5
William <wpanec@unomaha.edu>
2017-09-27 02:51:12 -0500

    Cosmetic change to user name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
                                    aria-expanded="false">
-                                    {{ Auth::user()->first_name }} <span class="caret"></span>
+                                    {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}<span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
