53be5c9d324ef7253165aa39f46c7f74b805df32
yuzesun <yuzesun@unomaha.edu>
2017-09-30 14:32:16 -0500

    Change css files structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,92 +3,86 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tagg</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
+<style>
+
+    html, body {
+        background-color: #71c4ff;
+        /*color: #636b6f;*/
+        /*font-family: 'Raleway', sans-serif;*/
+        /*font-weight: 100;*/
+        /*height: 100vh;*/
+        /*margin: 0;*/
+    }
+
+
+
+    /*.content {*/
+        /*text-align: center;*/
+    /*}*/
+
+    /*.title {*/
+        /*font-size: 84px;*/
+    /*}*/
+
+    /*.links > a {*/
+        /*color: #636b6f;*/
+        /*padding: 0 25px;*/
+        /*font-size: 12px;*/
+        /*font-weight: 600;*/
+        /*letter-spacing: .1rem;*/
+        /*text-decoration: none;*/
+        /*text-transform: uppercase;*/
+    /*}*/
+
+    /*.m-b-md {*/
+        /*margin-bottom: 30px;*/
+    /*}*/
+</style>
+
 <body>
-<div id="app">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+{{--<div class="flex-center position-ref full-height w3-top" style="background-color: #a6e1ec">--}}
+<div class="w3-top">
+    <div class="w3-bar w3-white w3-padding w3-card-2">
+        <a href="{{ url('/') }}" class="w3-bar-item w3-button">CommunityQ</a>
+        @if (Auth::guest())
+            <div class="w3-right">
+                <a href="{{ route('login') }}" class="w3-bar-item w3-button">Login</a>
+                <a href="{{ route('register') }}" class="w3-bar-item w3-button">Register</a>
+                <a href="donationrequest?orgId=1" class="w3-bar-item w3-button">Request Donation</a>
+            </div>
+        @else
+            <div class="w3-right">
+                <a href="donationrequest?orgId=1" class="w3-bar-item w3-button">Request Donation</a>
+                <a href="{{ url('/home') }}" class="w3-bar-item w3-button">
+                        {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}
                 </a>
+                <a href="users" class="w3-bar-item w3-button">Update Profile</a>
+                <a href="{{ route('reset-password') }}" class="w3-bar-item w3-button">Reset Password</a>
+                    <a href="{{ route('logout') }}" class="w3-bar-item w3-button"
+                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                          style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
             </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    &nbsp;
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Register</a></li>
-                        <li><a href="donationrequest?orgId=1">Request Donation</a></li>
-                        @else
-                            <li><a href="{{ url('/home') }}">Dashboard</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                   aria-expanded="false">
-                                    {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}<span
-                                            class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="users">Update Profile</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('reset-password') }}">
-                                            Reset Password
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                              style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                            @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    @yield('content')
+        @endif
+    </div>
 </div>
-<!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
+<br><br><br><br>
+@yield('content')
 
 </body>
-</html>
+
+
