19a49c3563cfa92860858dbcd8d64dce9192c579
gayanala <gayanala@unomaha.edu>
2017-10-01 17:20:28 -0500

    merged the fields of profile table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\DonationRequest;
 use App\User;
 use App\Organization;
 use Illuminate\Http\Request;
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,34 +11,22 @@
                         <table class="table table-striped table-bordered table-hover">
                             <thead>
                             <tr class="bg-info">
-                                <th>First Name</th>
-                                <th>Last Name</th>
+                                <th>Name</th>
                                 <th>Email</th>
-                                <th>Street Address 1</th>
-                                <th>Street Address 2</th>
-                                <th>City</th>
-                                <th>State</th>
-                                <th>Zip Code</th>
+                                <th>Address</th>
                                 <th>Phone Number</th>
                                 <th colspan="3">Actions</th>
                             </tr>
                             </thead>
                             <tbody>
-                            {{--@foreach ($users as $user)--}}
                                 <tr>
-                                    <td>{{ $user->first_name }}</td>
-                                    <td>{{ $user->last_name }}</td>
+                                    <td>{{ $user->first_name }} {{ $user->last_name }}</td>
                                     <td>{{ $user->email }}</td>
-                                    <td>{{ $user->street_address1 }}</td>
-                                    <td>{{ $user->street_address2 }}</td>
-                                    <td>{{ $user->city }}</td>
-                                    <td>{{ $user->state }}</td>
-                                    <td>{{ $user->zipcode }}</td>
+                                    <td>{{ $user->street_address1 }} {{ $user->street_address2 }} {{ $user->city }} {{ $user->state }} {{ $user->zipcode }}</td>
                                     <td>{{ $user->phone_number }}</td>
                                     <td><a href="{{route('users.edit',$user->id)}}" class="btn btn-warning"> Edit </a>
                                     </td>
                                 </tr>
-                            {{--@endforeach--}}
                             </tbody>
                         </table>
                         <div class="panel-heading"><h1>Add a User</h1></div>
