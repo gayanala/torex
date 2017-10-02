f4e70822f1fee0fb6cd4405491fec4bbf8e3c35d
Akhilesh Kontham <akontham@unomaha.edu>
2017-10-02 00:45:10 -0500

    added mailgun configuration -Akhilesh

--- database/migrations/2017_10_01_204403_modify_organizations_table.php
+++ database/migrations/2017_10_01_204403_modify_organizations_table.php
@@ -16,14 +16,15 @@ class ModifyOrganizationsTable extends Migration
         Schema::table('organizations', function (Blueprint $table) {
             /*if (Schema::hasColumn('organizations','state'))
             {*/
-                $table->dropColumn('state');
+            //s$table->dropForeign('organizations_state_foreign');
+            $table->dropColumn('state');
             //}
 
         });
         Schema::table('organizations', function (Blueprint $table) {
             $table->char('state',2)->after('city');
             $table->foreign('state')->references('state_code')->on('states')->onDelete('cascade');
-            $table->integer('organization_type_id')->after('org_name');
+            $table->integer('organization_type_id')->unsigned()->after('org_name');
             $table->foreign('organization_type_id')->references('id')->on('organization_types')->onDelete('cascade');
         });
     }
