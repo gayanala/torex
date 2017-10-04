d14a0feffccc7dbc07c783f51fe9337625b7c62f
gayanala <gayanala@unomaha.edu>
2017-10-04 12:41:44 -0500

    changed welcome page text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <div class="content">
         <div style="text-align:center;padding:170px;">
             <br><br><br><br><br>
-            <h1 style="font-size:250px">Q</h1>
+            <h1 style="font-size:150px">CommunityQ</h1>
         </div>
     </div>
 
