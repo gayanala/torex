3b52524fc84b975760ac44fa85fff7aabda41378
gayanala <gayanala@unomaha.edu>
2017-10-04 14:23:54 -0500

    removed comment in databaseseeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,13 +14,13 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->command->info("Roles table seeded");
 
-        /*$this->call(Security_questionsTableSeeder::class);
+        $this->call(Security_questionsTableSeeder::class);
         $this->call(StatesTableSeeder::class);
         $this->call(Organization_typesTableSeeder::class);
         $this->call(RequesterTypesTableSeeder::class);
         $this->call(Request_item_typesTableSeeder::class);
         $this->call(Request_item_purposesTableSeeder::class);
-        $this->call(Request_event_typesTableSeeder::class);*/
+        $this->call(Request_event_typesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->command->info("Users table seeded");
     }
