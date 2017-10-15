b75abddde1bf12680e334f2691d5bb0541c84ed1
William <wpanec@unomaha.edu>
2017-10-15 14:28:38 -0500

    Removed stray '-->' from rules gui

--- resources/views/rules/guirules.blade.php
+++ resources/views/rules/guirules.blade.php
@@ -2,7 +2,7 @@
 @section('header')
     <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script> -->
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="{{ asset('querybuilder/jquery/dist/jquery.js') }}"></script>
     <script type="text/javascript" src="{{ asset('querybuilder/bootstrap/dist/js/bootstrap.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.3.0/bootbox.min.js"></script>
