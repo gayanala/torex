6b1955c51b61e1d09abafee2ead59c295287fceb
William <wpanec@unomaha.edu>
2017-10-17 18:15:35 -0500

    Seeder tweeks

--- database/seeds/RulesTableSeeder.php
+++ database/seeds/RulesTableSeeder.php
@@ -14,7 +14,7 @@ class RulesTableSeeder extends Seeder
     {
         DB::table('rules')->delete();
         Rule::create(['id' => 1, 'rule_type_id' => 1, 'rule_owner_id' => 1, 'active' => true,
-            'rule' => "{'condition': 'AND','rules': [ {'id': 'amount','field': 'amount','type': 'double','input': 'number','operator': 'less','value': '500' }, {'id': 'needed_by_date','field': 'needed_by_date','type': 'date','input': 'text','operator': 'not_between','value': ['10/22/2017','10/28/2017'] }],'not': false,'valid': true}"
+            'rule' => "{  \"condition\": \"AND\",  \"rules\": [ {\"id\": \"dollar_amount\",\"field\": \"dollar_amount\",\"type\": \"double\",\"input\": \"number\",\"operator\": \"less\",\"value\": \"500\" }, {\"id\": \"needed_by_date\",\"field\": \"needed_by_date\",\"type\": \"date\",\"input\": \"text\",\"operator\": \"not_between\",\"value\": [  \"10/22/2017\",  \"10/28/2017\"] }  ],  \"not\": false,  \"valid\": true}"
             ]);
     }
 }
