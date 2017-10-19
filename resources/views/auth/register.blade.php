fb97639de8fec2896ea8693e32efb0a818b36429
punyasha <proul@unomaha.edu>
2017-10-19 18:36:30 -0500

    phone number us format

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -197,7 +197,7 @@
                             <div class="form-group{{ $errors->has('phone_number') ? ' has-error' : '' }}">
                                 <label for="phone_number" class="col-md-4 control-label">Phone Number <span style="color: red; font-size: 20px; vertical-align:middle;">*</span></label>
                                 <div class="col-md-6">
-                                    <input id="phone_number" type="text" pattern="[0-9]{10}" required title="Enter your 10 digit Phone Number" class="form-control" name="phone_number"
+                                    <input id="phone_number" type="text" pattern="\d{3}[\-]\d{3}[\-]\d{4}" required title="Enter your 10 digit Phone Number as 555-555-5555" class="form-control" name="phone_number"
                                            value="{{ old('phone_number') }}" placeholder="Phone Number" required
                                            autofocus>
                                     @if ($errors->has('phone_number'))
