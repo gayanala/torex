2c787a43b905239e1b509a6d3aff1162fb0de3a1
punyasha <proul@unomaha.edu>
2017-10-21 09:41:04 -0500

    error handling 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User not found.</title>
+</head>
+<body background="https://images.techhive.com/images/article/2016/04/error-thinkstock-100655502-primary.idge.jpg">
+<p>You broke the balance of the internet</p>
+</body>
+</html>
