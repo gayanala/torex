365e7ba6b7befefa26506cc47cad8b6fe7f1dbaf
punyasha <proul@unomaha.edu>
2017-10-22 13:27:54 -0500

    error image added

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,9 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>User not found.</title>
+    <style>
+        body, html {
+            height: 100%;
+            margin: 0;
+        }
+
+        .bg {
+            /* The image used */
+            background-image: url("{{ asset('img/error.jpg') }}");
+
+            /* Full height */
+            height: 70%;
+
+            /* Center and scale the image nicely */
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: contain;
+        }
+        p
+        {
+            text-align: center;
+            color: #9a161a;
+            text-decoration: underline;
+            text-shadow: 1px 1px red;
+            font-family: "Times New Roman", Georgia, Serif;
+            font-size: 50px;
+        }
+    </style>
+    <title>Page not found.</title>
 </head>
-<body background="https://images.techhive.com/images/article/2016/04/error-thinkstock-100655502-primary.idge.jpg">
-<p>You broke the balance of the internet</p>
+<body>
+<p>Oops ! Something went wrong. <br>
+    The Page you are searching for is not found !
+</p>
+<div class="bg"></div>
+
 </body>
 </html>
