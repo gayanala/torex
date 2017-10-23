3570a3fac0e0afd7cdd89589bb606df0139a5cce
skponton <skponton@unomaha.edu>
2017-10-23 16:47:38 -0500

    Uncommented needed code.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('CronJob:cronjob')
         //->twiceDaily(self::FIRST_RUN_HOUR, self::SECOND_RUN_HOUR);
-        //  ->everyFiveMinutes();
+        ->everyFiveMinutes();
 
     }
 
