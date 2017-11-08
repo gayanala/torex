5e2bcc834ae9288bdb62a5f8ea153542bd2b44c0
yuzesun <yuzesun@unomaha.edu>
2017-11-08 10:32:22 -0600

    Formatted Reminder Email

--- resources/views/emails/scheduledemail/pendingreminders.blade.php
+++ resources/views/emails/scheduledemail/pendingreminders.blade.php
@@ -1,13 +1,14 @@
 @component('mail::message')
-    # Hi, {{$event->name}}
 
-    You have {{$event->countPendingDonationRequests}} donation requests that are pending for your organization {{$event->organizationName}}
-    and going to be expired within 3 weeks. Please review the donation requests as soon as possible.
+# Hi, {{$event->name}}
 
-    @component('mail::button', ['url' => 'http://tagg-preprod.herokuapp.com/'])
-        Go to CharityQ
-    @endcomponent'
+You have {{$event->countPendingDonationRequests}} donation requests that are pending for your organization {{$event->organizationName}}
+and going to be expired within 3 weeks. Please review the donation requests as soon as possible.
 
-    Thanks,<br>
-    {{ config('app.name') }}
+@component('mail::button', ['url' => 'http://tagg-preprod.herokuapp.com/'])
+Go to CharityQ
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
 @endcomponent
