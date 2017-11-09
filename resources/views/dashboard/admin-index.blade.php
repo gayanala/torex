31f35db0710271144cb0065068befcf321e57140
gayanala <gayanala@unomaha.edu>
2017-11-09 14:01:51 -0600

    added status field

--- resources/views/dashboard/admin-index.blade.php
+++ resources/views/dashboard/admin-index.blade.php
@@ -196,7 +196,7 @@
                                     <tbody  style="text-align: center">
                                         @foreach ($organizations as $organization)
                                             <tr>
-                                                <td style="vertical-align: middle">status</td>
+                                                <td style="vertical-align: middle">{{ $organization->trial_ends_at->gte(\Carbon\Carbon::now()) ? 'Active' : 'Inactive' }}</td>
                                                 <td style="vertical-align: middle">{{ $organization->id }}</td>
                                                 <td style="vertical-align: middle">{{ $organization->org_name }}</td>
                                                 <td style="vertical-align: middle">${{ $organization->donationRequest->sum('dollar_amount') }}</td>
