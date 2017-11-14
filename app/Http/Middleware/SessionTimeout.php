d94704c72662ef61281263eac716d43090bc5a47
skponton <skponton@unomaha.edu>
2017-11-13 18:03:19 -0600

    Testing session timeout...

---
+++ app/Http/Middleware/SessionTimeout.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SanKp
+ * Date: 11/13/2017
+ * Time: 5:44 PM
+ */
+
+namespace App\Http\Middleware;
+
+
+use Closure;
+use Illuminate\Session\Store;
+
+class SessionTimeout {
+
+    protected $session;
+  //  protected $timeout = 1200;
+    protected $timeout = 120;
+    public function __construct(Store $session){
+        $this->session = $session;
+    }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $isLoggedIn = $request->path() != 'dashboard/logout';
+        if(! session('lastActivityTime'))
+            $this->session->put('lastActivityTime', time());
+        elseif(time() - $this->session->get('lastActivityTime') > $this->timeout){
+            $this->session->forget('lastActivityTime');
+            $cookie = cookie('intend', $isLoggedIn ? url()->current() : 'dashboard');
+            $email = $request->user()->email;
+            auth()->logout();
+            return message('You had not activity in '.$this->timeout/60 .' minutes ago.', 'warning', 'login')->withInput(compact('email'))->withCookie($cookie);
+        }
+        $isLoggedIn ? $this->session->put('lastActivityTime', time()) : $this->session->forget('lastActivityTime');
+        return $next($request);
+    }
+
+}
