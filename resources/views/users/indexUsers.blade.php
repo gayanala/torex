dd0937985bf5f4959b9ae6f26aa17b289f8336bb
abhisura <ssura@unomaha.edu>
2017-11-27 19:08:22 -0600

    fixed the issues in user index
    user edits page will now not be seeing the city and zipcode

--- resources/views/users/indexUsers.blade.php
+++ resources/views/users/indexUsers.blade.php
@@ -32,13 +32,15 @@
                                 <td style="vertical-align: middle">{{ $user->first_name }} {{ $user->last_name }}</td>
                                 <td style="vertical-align: middle">{{ $user->roles[0]->name }}</td>
                                 <td style="vertical-align: middle">{{ $user->organization->org_name }}</td>
-<<<<<<< HEAD
+
                                <!-- <td style="vertical-align: middle">{{ $user->organization->city }}, {{ $user->organization->zipcode }}</td>  -->
+
                                 <td style="vertical-align: middle"><a href="{{route('edituser',$user->id)}}"
-=======
-                                <td style="vertical-align: middle">{{ $user->organization->city }}, {{ $user->organization->zipcode }}</td>
+
+                              
+
                                 <td style="vertical-align: middle"><a href="{{route('edituser',encrypt($user->id))}}"
->>>>>>> 083cafa9b749c6a240211a00367fa4a4211de148
+
                                                                       class="btn btn-warning"> Edit </a>
                                 </td>
                             </tr>
