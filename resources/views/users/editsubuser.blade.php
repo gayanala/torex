4bfc7bf0db34d130eec8ae310e9c6060ea04db3d
punyasha <proul@unomaha.edu>
2017-12-04 19:23:03 -0600

    edit user and org heading

--- resources/views/users/editsubuser.blade.php
+++ resources/views/users/editsubuser.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Update Profilez</div>
+                    <div class="panel-heading"><h1 style="text-align: left;font-weight: bold;">Update Profile</h1></div>
 
                     <div class="panel-body">
 
