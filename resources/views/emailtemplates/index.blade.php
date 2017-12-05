2611ec0c28a41550f439366375c9fb65d980fb07
punyasha <proul@unomaha.edu>
2017-12-05 09:12:21 -0600

    email template header

--- resources/views/emailtemplates/index.blade.php
+++ resources/views/emailtemplates/index.blade.php
@@ -5,7 +5,7 @@
     <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header text-center" style="font-size:20px;font-weight: 900;">Search Donations</h1>
+                <h1 class="page-header text-center" style="font-size:20px;font-weight: 900;">Email Templates</h1>
 
             </div>
             <!-- /.col-lg-12 -->
@@ -19,7 +19,7 @@
                 <div class="panel panel-default">
                     {{ csrf_field() }}
                     <div class="panel-heading">
-                        <h1 style="text-align: left;font-weight: bold;">Edit Email Templates Here</h1>
+                        <h1 style="text-align: left;font-weight: bold;">Edit Templates Here</h1>
                     </div>
 
                     <div class="panel-body">
