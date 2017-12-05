9294aafdd6bd0f35f559941d813c56f4e0e3ad1c
punyasha <proul@unomaha.edu>
2017-12-04 19:32:17 -0600

    email template header

--- resources/views/emailtemplates/edit.blade.php
+++ resources/views/emailtemplates/edit.blade.php
@@ -47,7 +47,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Update Template</div>
+                    <div class="panel-heading"><h1 style="text-align: left;font-weight: bold;">Update Template</h1></div>
 
                     <div class="panel-body">
 
