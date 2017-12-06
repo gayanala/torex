de327f9ee67a596d924d96ffa9c29441d57471dc
om1993 <odarshane@unomaha.edu>
2017-12-05 22:07:51 -0600

    BG-1333

--- resources/views/rules/help.blade.php
+++ resources/views/rules/help.blade.php
@@ -8,9 +8,12 @@
     <div class="container">
         <div>
 <h1 style="left-padding:20%;"><b>Business Rules Help</b></h1><br>
-                <iframe src="{{ asset('files/business_rules_help.pdf') }}" width="100%" height="670px" scrolling="Yes"
-                        style=" border:solid;left-paddinf:20%;"></iframe>
+            <iframe src="{{ asset('files/business_rules_help.pdf') }}" width="100%" class="rulesifr" scrolling="Yes"
+                    style=" border:solid;left-paddinf:20%;"></iframe>
         </div>
+        <script type="text/javascript" language="javascript">
+            $('.rulesifr').css('height', $(window).height() + 'px');
+        </script>
     </div>
 
 @endsection
