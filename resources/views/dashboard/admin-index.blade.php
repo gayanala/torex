afd7139828073c0d2becd577823745ef44a4429f
arkontham <akontham@unomaha.edu>
2017-12-07 20:26:26 -0600

    BG-1361 and admin dashboard changes

--- resources/views/dashboard/admin-index.blade.php
+++ resources/views/dashboard/admin-index.blade.php
@@ -43,28 +43,35 @@
                     <div class="panel panel-red">
                         <div class="panel-heading" style="background-color: #b69da8;">
                             <div class="row">
-                                <div class="col-xs-3" style="padding-bottom: 15px;">
-                                    <i class="fa fa-university fa-5x" style="color: white"></i>
-                                </div>
-                                <div class="col-xs-9 text-left">
-                                    <div style="color: white;font-size: 15px;font-weight: bolder;"> AVERAGE AMOUNT
-                                        DONATED : <span class="huge"
-                                                        style="font-weight: bold; font-size: 20px;">${{ $avgAmountDonated }}</span></div>
-                                </div>
-                                <div class="col-xs-9 text-left">
-                                    <div style="color: white;font-size: 15px;font-weight: bolder;"> ACTIVE CUSTOMERS :
-                                        <span class="huge"
-                                              style="font-weight: bold; font-size: 20px;">{{ $userCount }}</span></div>
-                                </div>
-                                <div class="col-xs-9 text-left">
-                                    <div style="color: white;font-size: 15px;font-weight: bolder;"> ACTIVE LOCATIONS :
-                                        <span class="huge"
-                                              style="font-weight: bold; font-size: 20px;">{{ $numActiveLocations }}</span></div>
-                                </div>
+
+                                <table border="0" style="color: white;font-size: 15px;">
+                                <tr>
+                                    <td rowspan="3">
+                                        <div class="col-xs-3" style="padding-bottom: 15px;">
+                                            <i class="fa fa-university fa-5x"></i>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div style="font-weight: lighter">AVG AMOUNT DONATED</div>
+                                    </td>
+                                    <td>
+                                        <div class="huge" style="font-weight: bolder; font-size: 20px; color: white">${{ $avgAmountDonated }}</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><div style="color: white;font-size: 15px;font-weight: lighter;"> ACTIVE CUSTOMERS :</div></td>
+                                    <td><div class="huge" style="font-weight: bold; font-size: 20px">{{ $userCount }}</div></td>
+
+                                </tr>
+                                    <td><div style="color: white;font-size: 15px;font-weight: lighter;"> ACTIVE LOCATIONS :</div></td>
+                                    <td><div class="huge" style="font-weight: bold; font-size: 20px">{{ $numActiveLocations }}</div></td>
+
+
+                                </table>
                             </div>
                         </div>
                     </div>
-                </div>
+            </div>
             <div class="col-lg-4 col-md-6">
                     <div class="panel panel-red">
                         <div class="panel-heading" style="background-color: #a5c5bd;">
